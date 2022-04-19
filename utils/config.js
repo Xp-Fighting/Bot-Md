ad0e4b638313dd0edd4ae5f0ee27030108bbe052
Fixee <103982241+ZennIXZ@users.noreply.github.com>
2022-04-19 23:17:01 +0700

    Create config.js

---
+++ utils/config.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zeks: 'https://api.zeks.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://api.zeks.xyz': 'BIKIN BANG',
+}
